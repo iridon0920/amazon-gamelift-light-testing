94a0f0a347e52b5c927e4ca01fc559f38624cf65
iridon0920 <iridon0920@gmail.com>
2024-11-18 21:49:19 +0900

    Add: GameLift SDKの設定をCmakeListsに作成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+#define GAMELIFT_USE_STD 1
 #include <iostream>
+#include <aws/gamelift/server/GameLiftServerAPI.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std::cout << Aws::GameLift::Server::GetSdkVersion().GetResult() << std::endl;
+
     return 0;
 }
